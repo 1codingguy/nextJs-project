f4ec8b3e5318798e17d2edbaae5b74de4ecc0645
1codingguy <1codingguy@gmail.com>
2023-12-24 11:08:14 +0700

    generate static pages from slugs

--- dj-events-frontend/app/events/[slug]/page.tsx
+++ dj-events-frontend/app/events/[slug]/page.tsx
@@ -1,17 +1,34 @@
-'use client'
+import { EventItemProps } from '@/app/components/EventItem'
+import { API_URL } from '@/app/config'
 
-import { useParams, useRouter } from 'next/navigation'
+// Dynamic segments not included in generateStaticParams are generated on demand
+// if false return 404 page
+export const dynamicParams = true
 
-const DynamicSlug = () => {
-  const params = useParams()
-  const router = useRouter()
+export async function generateStaticParams() {
+  const evts = await fetch(`${API_URL}/api/events/`).then(res => res.json())
 
-  const handleClick = () => {
-    console.log('clicked')
-    router.push('/')
-  }
+  const returnItem = evts.events.events.map((event: EventItemProps) => ({
+    slug: event.slug,
+  }))
 
-  return <h1 onClick={handleClick}>DynamicSlug</h1>
+  return returnItem
 }
 
-export default DynamicSlug
+// 
+async function getEvent(slug: string) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const { event } = await res.json()
+  return event
+}
+
+const EventPage = async ({ params }: { params: { slug: string } }) => {
+  // `param` is provided by Next.js in this default function signature.
+  // Not in getEvent()
+
+  const event: EventItemProps = await getEvent(params.slug)
+
+  return <h1>{event.name}</h1>
+}
+
+export default EventPage
