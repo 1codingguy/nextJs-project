bf389d775f31203bb1ac220aadc357523784456d
1codingguy <1codingguy@gmail.com>
2023-12-29 12:48:12 +0700

    events page pagination

--- dj-events-frontend/app/events/page.tsx
+++ dj-events-frontend/app/events/page.tsx
@@ -1,34 +1,94 @@
+'use client'
+
 import { API_URL } from '@config/index'
 import EventItem, { EventItemProps } from '@components/EventItem'
-import { revalidateTag } from 'next/cache'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import qs from 'qs'
+import Link from 'next/link'
+import { EVENTS_PER_PAGE } from '@config/index'
 
-async function getEvents() {
-  const res = await fetch(`${API_URL}/api/events?populate=*`, {
-    next: { tags: ['all-events'] },
-  }).then(res => res.json())
-  return res.data
+type dataProps = {
+  id: number
+  attributes: EventItemProps
 }
 
-export default async function EventsPage() {
-  revalidateTag('all-events')
-  const data = await getEvents()
+type metaProps = {
+  pagination: {
+    page: number
+    pageSize: number
+    pageCount: number
+    totalCount: number
+  }
+}
+
+type returnResult = {
+  data: dataProps[]
+  meta: metaProps
+}
+
+async function getEvents(page: string) {
+  const query = qs.stringify(
+    {
+      pagination: {
+        page: page,
+        pageSize: EVENTS_PER_PAGE,
+      },
+      populate: '*',
+    },
+    {
+      encodeValuesOnly: true, // prettify URL
+    }
+  )
+
+  const res: returnResult = await fetch(`${API_URL}/api/events?${query}`).then(
+    res => res.json()
+  )
+
+  return res
+}
+
+export default function EventsPage() {
+  const searchParams = useSearchParams()
+  const page = searchParams.get('page') ?? '1'
+
+  const [data, setData] = useState<dataProps[] | undefined>()
+  const [metaData, setMetaData] = useState<metaProps | undefined>()
+
+  useEffect(() => {
+    getEvents(page).then(data => {
+      setData(data.data)
+      setMetaData(data.meta)
+    })
+  }, [page])
+
+  if (data === undefined) return <h3>No events to show</h3>
+
+  const { pagination: { page: currentPage = 1, pageCount = 1 } = {} } =
+    metaData || {}
 
   return (
     <div>
       <h1>Upcoming Events</h1>
-      {data.length === 0 && <h3>No events to show</h3>}
 
-      {data.map(
-        ({
-          id,
-          attributes: evt,
-        }: {
-          id: number
-          attributes: EventItemProps
-        }) => (
-          <EventItem key={id} evt={evt} />
-        )
-      )}
+      {data.map(({ id, attributes: evt }: dataProps) => (
+        <EventItem key={id} evt={evt} />
+      ))}
+
+      {
+        <div>
+          {currentPage > 1 && (
+            <Link href={`/events?page=${currentPage - 1}`}>
+              <button className='btn-secondary'>Prev</button>
+            </Link>
+          )}
+          {currentPage < pageCount && (
+            <Link href={`/events?page=${currentPage + 1}`}>
+              <button className='btn-secondary'>Next</button>
+            </Link>
+          )}
+        </div>
+      }
     </div>
   )
 }
